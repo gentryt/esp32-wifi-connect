53922da970cedba65571dc1913c27931847fc047
gentryt <gentrytl@gmail.com>
2020-12-16 05:43:42 -0600

    Added DTH1 and Cap Moisture Sensors

--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,43 @@
 #include <Preferences.h>
 #include <PubSubClient.h>
 #include <WiFi.h>
+#include <NTPClient.h>
+#include <SimpleDHT.h>
+#include <ArduinoJson.h>
+
+int pinDHT11 = 2;
+SimpleDHT11 dht11(pinDHT11);
+const int AirValue = 2640;
+const int WaterValue = 1650;
+const int SensorPin = 34;
 
 String deviceName = "AutoWater_1";
 Preferences settings;
 const char* mqttServer = "192.168.1.75";
 WiFiClient espClient;
+WiFiUDP ntpUDP;
 PubSubClient client(espClient);
+NTPClient timeClient(ntpUDP,21600);
 
 #define network "2.4GHz_Modem"
 #define pw "L30N@rd01"
 #define timeout 20000
 
 #define REPORT_TOPIC "garden/AutoWater_1/report"
+#define REPORT_STATUS "garden/1/status"
 
 void receivedCallBack(char* topic, byte* payload, unsigned int length){
   Serial.print("Message received: ");
   Serial.println(topic);
   Serial.print("Payload: ");
 
+  String payloadText = "";
   for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
+    payloadText += (char)payload[i];
   }
   
+  Serial.println( payloadText);
   Serial.println();
-
-  // Do work here
 }
 
 void  connectWifi(){
@@ -82,20 +94,64 @@ void setupPreferences(){
   settings.end();
 }
 
+void reportStatus(){
+  Serial.println("==========================================");
+  Serial.println("Sample DHT11...");
+
+  byte temperature = 0;
+  byte humidity = 0;
+
+  int err = SimpleDHTErrSuccess;
+  if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    Serial.print("Read DHT11 failed, err="); 
+    Serial.println(err);
+    delay(1000);
+    return;
+  }
+
+  int soilMoisture = 0;
+  int soilMoisturePercent = 0;
+  soilMoisture = analogRead(SensorPin);
+  soilMoisturePercent = map(soilMoisture, AirValue, WaterValue, 0,100);
+  int p = 0;
+  if (soilMoisturePercent > 100){
+    p = 100;
+  }else if(soilMoisturePercent <= 0){
+    p = 0;
+  }else{
+    p = soilMoisturePercent;
+  }
+
+  DynamicJsonDocument doc(128);
+  doc["time"] = String(timeClient.getFormattedTime());
+  doc["temp"] = String(temperature);
+  doc["hum"] = String(humidity);
+  doc["moisture"] = String(soilMoisture);
+  doc["moisturePercent"] = String(p);
+
+  char buffer[256];  
+  serializeJson(doc, buffer);
+  Serial.println(buffer);
+  client.publish(REPORT_STATUS, buffer);
+
+  delay(2500);
+}
+
 void setup() {
   Serial.begin(115200);
   connectWifi();
   client.setServer(mqttServer, 1883);
   client.setCallback(receivedCallBack);
+  timeClient.begin();
 }
 
 void loop() {
+  timeClient.update();
   if (!client.connected()){
     connectMqtt();
   }
   client.loop();
 
+  reportStatus();
 
-
-  
 }
